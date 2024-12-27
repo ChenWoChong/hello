23489974803d700e57c464ed18c0a09cb9ab2432
chenwochong <chenwochong@163.com>
2024-12-27 15:06:00 +0800

    typo

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,8 @@
 #[allow(dead_code)]
 #[allow(unused_assignments)]
 fn main() {
-    let mut a=10u32;
-    let mut b = &mut a;
-    let mut c = &mut b;
-    let d = &mut c;
-    ***d = 88;
-    println!("{d}");
-
-    let mut s = String::from("hh");
-    println!("{s}");
-    foo(&mut s);
-    println!("{s}");
-
-    let mut a = 10u32;
-    let b = &a;
-    println!("{b}");
-    a=33;
-}
-
-fn foo(s: &mut String) {
-    s.push_str("hello world.");
+    let s1: &'static str = "hello";
+    let s2 = s1.to_string();
+    let s5 = &s2[..6];
+    // println!("{s5}");
 }
