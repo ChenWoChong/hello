0979598ecd56802db728c7dced49c5718cc13388
chenwochong <chenwochong@163.com>
2024-12-27 18:22:33 +0800

    typo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-#[allow(unused_variables)]
-#[allow(dead_code)]
-#[allow(unused_assignments)]
 fn main() {
-    let s1: &'static str = "hello";
-    let s2 = s1.to_string();
-    let s5 = &s2[..6];
-    // println!("{s5}");
+    let a_vec: Vec<u8> = vec![1, 2, 3, 4, 5, 6];
+    let a_slice = &a_vec[0..5];
+    let _aa_vec = a_slice.to_vec();
+    let _aaa_vec = a_slice.to_owned();
+
+    let s = String::from("foo");
+    assert_eq!(s.as_str(), &s[..]);
+
+    assert_eq!(s.as_bytes(), &[102, 111, 111]);
+    assert_eq!(&[1, 2, 3, 4, 5, 6], a_vec.as_slice());
+    let v: Vec<u32> = vec![1, 3, 3, 4];
+    foo(&v);
+    let a = v.as_slice();
+    foo(a);
+}
+
+fn foo(s: &[u32]) {
+    println!("{:?}", s);
 }
