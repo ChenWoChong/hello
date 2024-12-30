617e66bd5b1093f11b6d537ff7abf1b1df6073ca
chenwochong <chenwochong@163.com>
2024-12-30 18:44:42 +0800

    test &&data

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-use std::rc::Rc;
-
 fn main() {
-    types_impl_copy_trait()
-}
+    let data: Vec<i32> = vec![1, 2, 3, 4];
+    let data1 = &data;
+    println!(
+        "addr of value:\t{:p} ({:p}),\naddr of  data:\t{:p},\naddr of data1:\t{:p},\naddr of   vec:\t{:p} ",
+        &data, data1, &&data, &data1, data.as_ptr()
+    );
 
-fn is_copy<T: Copy>() {}
+    // 打印 data 的地址（存储在栈上）
+    println!("Address of data on stack:\t{:p}", &data);
 
-fn types_impl_copy_trait() {
-    is_copy::<bool>();
-    is_copy::<char>();
+    // 打印 Vec 的堆内存地址（数据存储在堆上）
+    let ptr = data.as_ptr();
+    println!("Address of Vec's heap memory:\t{:?}", ptr);
 
-    // is_copy::<String>();
-
-    is_copy::<fn(Rc<i32>)>();
-
-    is_copy::<*const String>();
-    is_copy::<*mut String>();
-
-    is_copy::<&[Vec<u8>]>();
-    // is_copy::<[Vec<u8>]>();
-
-    is_copy::<[u8; 4]>();
-    // is_copy::<[String; 4]>();
-
-    is_copy::<(&str, &str)>();
-    // is_copy::<(str, str)>();
+    // 打印 Vec 中每个元素的地址
+    for (i, element) in data.iter().enumerate() {
+        println!("Address of element {}: {:p}", i, element);
+    }
 }
