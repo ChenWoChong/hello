10b283e8be7c0e01e4645c5d10a5ea65ec8f1f69
chenwochong <chenwochong@163.com>
2024-12-30 11:54:00 +0800

    add tuple

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
+use std::rc::Rc;
+
 fn main() {
-    let a_vec: Vec<u8> = vec![1, 2, 3, 4, 5, 6];
-    let a_slice = &a_vec[0..5];
-    let _aa_vec = a_slice.to_vec();
-    let _aaa_vec = a_slice.to_owned();
-
-    let s = String::from("foo");
-    assert_eq!(s.as_str(), &s[..]);
-
-    assert_eq!(s.as_bytes(), &[102, 111, 111]);
-    assert_eq!(&[1, 2, 3, 4, 5, 6], a_vec.as_slice());
-    let v: Vec<u32> = vec![1, 3, 3, 4];
-    foo(&v);
-    let a = v.as_slice();
-    foo(a);
+    types_impl_copy_trait()
 }
 
-fn foo(s: &[u32]) {
-    println!("{:?}", s);
+fn is_copy<T: Copy>() {}
+
+fn types_impl_copy_trait() {
+    is_copy::<bool>();
+    is_copy::<char>();
+
+    // is_copy::<String>();
+
+    is_copy::<fn(Rc<i32>)>();
+
+    is_copy::<*const String>();
+    is_copy::<*mut String>();
+
+    is_copy::<&[Vec<u8>]>();
+    // is_copy::<[Vec<u8>]>();
+
+    is_copy::<[u8; 4]>();
+    // is_copy::<[String; 4]>();
+
+    is_copy::<(&str, &str)>();
+    // is_copy::<(str, str)>();
 }
