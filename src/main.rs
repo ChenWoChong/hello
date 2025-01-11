f8001fc5246ad82da0d10ac8f1f790b997a34794
chenwochong <chenwochong@163.com>
2025-01-11 11:37:13 +0800

    typos

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,11 @@
+use std::collections::HashMap;
+
 fn main() {
-    let u = User {
-        active: false,
-        username: String::from("haha"),
-        email: String::from("sdsf"),
-        sign_in_count: 88,
-    };
-    println!("{:#?}", u);
-
-    let u2 = User {
-        username: String::from("ttt"),
-        email: String::from("xxxx"),
-        ..u
-    };
-
-    let members = vec![u, u2];
-    let c = Class{
-        s_num:6,
-        g_num:6,
-        year: String::from("2025"),
-        members,
-    };
-    println!("{:#?}", c);
-
-    let _color = Color(0,0,0);
-    let _mi32 = Myi32(1);
-    
+    let mut map = HashMap::new();
+    map.insert("a", 1);
+    explain("a", map);
 }
 
-#[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+fn explain<K, V>(s: &str, map: HashMap<K, V>) {
+    println!("{} {} {}", s, map.len(), map.capacity());
 }
-
-#[derive(Debug)]
-struct Class {
-    s_num: u32,
-    g_num: u32,
-    year: String,
-    members: Vec<User>,
-}
-
-struct Color(i32,i32,i32);
-struct Myi32(i32);
