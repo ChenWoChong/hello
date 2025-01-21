f7d015f357119c4169882ff7512f18c6c69c07b0
chenwochong <chenwochong@163.com>
2025-01-21 15:11:41 +0800

    test option to result

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,4 @@
 fn main() {
-    let mut a = String::from("hello");
-    let mut b = "halo".to_string();
-
-    let mut c = || {
-        a.push_str(" world!");
-        println!("c {}", a);
-    };
-
-    let mut c1 = move || {
-        b.push_str("!!@");
-        println!("c1 {b}");
-    };
-
-    c();
-    c1();
-
-    c();
-    c1();
-    println!("--------------");
-
-    call_mut(&mut c);
-    call_mut(&mut c1);
-    call_mut(&mut c);
-    call_mut(&mut c1);
-    println!("--------------");
-
-
-    call_once(c);
-    call_once(c1);
-    // 不可以调用两次
-    // call_once(c); 
-    // call_once(c1);
-
-}
-
-fn call_mut(c: &mut impl FnMut()) {
-    c();
-}
-
-fn call_once(c: impl FnOnce()) {
-    c();
+    let t = Some("foo");
+    assert_eq!(t.ok_or(Err::<&str, &str>("kkk")), Ok("foo"));
 }
