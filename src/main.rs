62252c4f6366563cd01e3206af74dbf73cce1c5a
chenwochong <chenwochong@163.com>
2025-01-22 11:47:30 +0800

    rust 10

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,35 @@ fn main() {
     np.echo();
     let np = p.add(38);
     np.echo();
+
+    let c = Ctype;
+    c.play(10u32);
 }
 
-trait TraitA<T> {}
+trait TraitA<T = Self> {
+    fn func(_t: T) {}
+}
+
+trait TraitB<T = i32> {
+    fn func2(_t: T) {}
+}
+
+struct SomeType;
+impl TraitA for SomeType {
+    fn func(_t: SomeType) {}
+}
+
+impl TraitB for SomeType {
+    fn func2(_t: i32) {}
+}
+
+impl TraitA<String> for SomeType {
+    fn func(_t: String) {}
+}
+
+impl TraitB<String> for SomeType {
+    fn func2(_t: String) {}
+}
 
 struct AType {}
 impl TraitA<i32> for AType {}
@@ -58,4 +84,22 @@ impl Point {
     fn echo(&self) {
         println!("x is {}, y is {}", self.x, self.y);
     }
+}
+
+use std::fmt::{Debug, Display};
+
+trait TraitC<T>
+where
+    T: Debug,
+{
+    fn play(&self, _t: T);
+}
+
+struct Ctype;
+
+impl<T> TraitC<T> for Ctype
+where
+    T: Debug + Display,
+{
+    fn play(&self, _t: T) {}
 }
