8c9ebcbe073ee505fbb74a70a5cf295e3c6ded45
chenwochong <chenwochong@163.com>
2025-01-24 16:22:02 +0800

    rust 11

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,246 @@
 fn main() {
-    let _a = doit0();
+    let _c = Color::default();
+    let _c = <Color>::default();
+    let _c: Color = Default::default();
+    let _c = Some(Color::default());
 
-    let _a = doit::<Atype>();
-    let _b = doit::<Btype>();
-    let _c = doit::<Ctype>();
+    let p = Point::default();
+    assert_eq!(p.x, 0);
+    assert_eq!(p.y, 0);
+    println!("{}", p);
+    let ps = format!("{}", p);
+    assert_eq!(ps, "0 0");
+    println!("{:#?}", p);
 
-    let _a = doit2();
+    let p = <Point>::default();
+    let q = <Point>::default();
+    assert_eq!(teq(p, q), true);
+    assert_eq!(teqc(vec![p], vec![q]), true);
 
-    let _a = doit3(1);
+    example_bteeset();
+    example_ord(vec![<Point>::default(), Point { x: 1, y: 1 }]);
 
-    doit4(&Atype);
-    doit4(&Btype);
-    doit4(&Ctype);
+    let p = Point { x: 3, y: 8 };
+    let q = Point { x: 1, y: 1 };
+    println!("{}", p + q);
+    println!("{}, {}", p, p + 3);
+    println!("{}", p);
 
-    let a = Atype;
-    let b = Btype;
-    let c = Ctype;
-    let _v: Vec<&dyn B> = vec![&a, &b, &c];
+    let a = Atype {
+        num: 10,
+        avec: vec![10, 10, 10],
+    };
+
+    let mut b = a.clone();
+    b.num = 1000;
+    b.avec[0] = 11;
+    b.avec[1] = 22;
+    b.avec[2] = 33;
+    println!("{:?}", a);
+    println!("{:?}", b);
+
+    let a = "123456";
+    let b = a.to_owned();
+    println!("{:?}", b);
+
+    let range = 0..10;
+    let cnt = range.len();
+    println!("{}", cnt);
+    let get_cnt = || range.count();
+    assert_eq!(get_cnt(), 10);
+
+    let nums = vec![0, 4, 2, 8, 10, 7, 15, 18, 13];
+    let mut min = i32::MIN;
+    let ascending = nums
+        .into_iter()
+        .filter(|&n| {
+            if n <= min {
+                false
+            } else {
+                min = n;
+                true
+            }
+        })
+        .collect::<Vec<i32>>();
+    println!("{:?}", ascending);
+
+    let nums = vec![0, 4, 2, 8, 10, 7, 15, 18, 13];
+    let min = 9;
+    let gt_9 = nums.into_iter().filter(|&n| n <= min).collect::<Vec<_>>();
+    println!("{:?}", gt_9);
+    println!("{:?}", gt_9);
+
+    let mut fp: fn(i32) -> i32 = add_one;
+    assert_eq!(fp(1), 2);
+
+    fp = |x| x * x;
+    assert_eq!(fp(3), 9);
+
+    let p = Point::from((8, 8));
+    println!("{}", p);
+    let p = Into::<Point>::into((9,9));
+    println!("{}", p);
+
 }
 
-#[derive(Debug)]
-struct Atype;
-struct Btype;
-struct Ctype;
-
-trait A {
-    fn new() -> Self;
+pub trait Default {
+    fn default() -> Self;
 }
 
-impl A for Atype {
-    fn new() -> Self {
-        Atype
+#[allow(dead_code)]
+pub struct Color(u8, u8, u8);
+impl Default for Color {
+    fn default() -> Self {
+        Color(0, 0, 0)
     }
 }
 
-impl A for Btype {
-    fn new() -> Self {
-        Btype
+#[derive(Default, Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Point {
+    x: u8,
+    y: u8,
+}
+
+impl std::fmt::Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.x, self.y)
     }
 }
 
-impl A for Ctype {
-    fn new() -> Self {
-        Ctype
+pub trait ToString {
+    fn to_string(&self) -> String;
+}
+
+pub trait D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
+}
+
+impl ToString for Point {
+    fn to_string(&self) -> String {
+        format!("{}", self)
     }
 }
 
-fn doit0() -> Box<dyn std::fmt::Debug> {
-    let a = Atype;
-    Box::new(a)
-}
+#[cfg(test)]
+mod test {
+    use super::*;
 
-fn doit<T: A>() -> T {
-    T::new()
-}
+    #[test]
+    fn test_point() {
+        let p = Point::default();
+        assert_eq!(format!("{}", p), "0 0");
+        assert_eq!(format!("{}", p), ToString::to_string(&p));
+        assert_eq!(format!("{}", p), std::string::ToString::to_string(&p));
+    }
 
-fn doit2() -> impl A {
-    Atype
-}
-
-trait B {}
-impl B for Atype {}
-impl B for Btype {}
-impl B for Ctype {}
-
-fn doit3(n: i32) -> Box<dyn B> {
-    if n == 0 {
-        Box::new(Atype)
-    } else if n == 1 {
-        return Box::new(Btype);
-    } else {
-        return Box::new(Ctype);
+    #[test]
+    fn test_pp() {
+        assert_eq!(format!("{}", <Point>::default()), "0 0");
     }
 }
 
-fn doit4(_a: &dyn B) {}
+fn teq(p: Point, q: Point) -> bool {
+    p == q
+}
 
-pub struct S(String);
+fn teqc<T: PartialEq>(p: Vec<T>, q: Vec<T>) -> bool {
+    p == q
+}
 
-pub trait Test {
-    fn tt(S(t): S) {
-        println!("{}", t);
+fn example_bteeset() {
+    let mut bs = std::collections::BTreeSet::<Point>::new();
+    let p = <Point>::default();
+    bs.insert(p);
+}
+
+fn example_ord<T: Ord>(mut sorts: Vec<T>) -> Vec<T> {
+    sorts.sort();
+    sorts
+}
+
+pub trait Ad<Rhs = Self> {
+    type Output;
+    fn add(&self, rhs: Rhs) -> Self::Output;
+}
+
+impl std::ops::Add<Point> for Point {
+    type Output = Point;
+    fn add(self, rhs: Point) -> Self::Output {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl std::ops::Add<i32> for Point {
+    type Output = Point;
+    fn add(mut self, rhs: i32) -> Self::Output {
+        self.x += rhs as u8;
+        self.y += rhs as u8;
+        self
+    }
+}
+
+pub trait Cl {
+    fn clone(&self) -> Self;
+}
+
+pub trait Cp: Cl {}
+
+#[derive(Clone, Debug)]
+struct Atype {
+    num: i32,
+    avec: Vec<u32>,
+}
+
+pub trait MyDeref {
+    type Target: ?Sized;
+    fn deref(&self) -> &Self::Target;
+}
+
+pub trait MyDrop {
+    fn drop(&mut self);
+}
+
+pub trait MyFnOnce<Args> {
+    type Output;
+    fn call_once(self, args: Args) -> Self::Output;
+}
+
+pub trait MyFnMut<Args>: MyFnOnce<Args> {
+    // type Output;
+    fn all_mut(&mut self, args: Args) -> Self::Output;
+}
+
+pub trait MyFn<Args>: MyFnMut<Args> {
+    // type Output;
+    fn call(&self, args: Args) -> Self::Output;
+}
+
+pub fn add_one(n: i32) -> i32 {
+    n + 1
+}
+
+pub trait MyFrom<T> {
+    fn from(t: T) -> Self;
+}
+
+pub trait MyInto<T> {
+    fn into(self) -> T;
+}
+
+impl<T, U> MyInto<U> for T
+where
+    U: MyFrom<T>,
+{
+    fn into(self) -> U {
+        U::from(self)
+    }
+}
+
+impl From<(u8, u8)> for Point {
+    fn from((x, y): (u8, u8)) -> Self {
+        Point { x, y }
     }
 }
