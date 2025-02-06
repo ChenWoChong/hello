d6b5a5504fe0dc7d063cb20047979a9bd4441e43
chenwochong <chenwochong@163.com>
2025-02-06 17:18:21 +0800

    rust 12

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 fn main() {
     let _a = foo();
     let _a = foo1();
@@ -10,11 +12,63 @@ fn main() {
 
     let mut ba = foo2();
     ba.play();
-    *ba = Point{ x: 100, y: 100 };
+    *ba = Point { x: 100, y: 100 };
     ba.play();
+
+    let mut anb = ba.clone();
+    *anb = Point { x: 4, y: 6 };
+    anb.play();
+    let y = &anb;
+    y.play();
+    let my = &mut anb;
+    **my = Point { x: 3, y: 5 };
+    anb.play();
+
     let a = *ba;
     // println!("{:?}", ba);
     println!("{:?}", a);
+
+    let mut ba = foo2();
+    ba.play_ref();
+    ba.play_mutref();
+    ba.play_own();
+    let ba = foo2();
+    ba.play_boxed();
+
+    let a = Box::new(Atype {});
+    doit(a);
+    let b = Box::new(Btype {});
+    doit(b);
+    let c = Box::new(Ctype {});
+    doit(c);
+
+    let _i = Mystruct {
+        a: Box::new(Atype {}),
+    };
+    let _i = Mystruct {
+        a: Box::new(Btype {}),
+    };
+    let _i = Mystruct {
+        a: Box::new(Ctype {}),
+    };
+    let _b = Mystruct {
+        a: Box::new(Ctype {}),
+    };
+
+    let a = std::sync::Arc::new(Point { x: 1, y: 2 });
+    let an = a.clone();
+    a.play();
+    an.play();
+    let ann = an.clone();
+    ann.play();
+
+    let c = std::sync::Arc::new(Point { x: 1, y: 2 });
+    c.play();
+    c.play_ref();
+    // c.play_mutref();
+    // c.play_own();
+    c.play_arc();
+    // c.play();
 }
 
 fn foo() -> String {
@@ -29,7 +83,7 @@ fn foo1() -> Box<u32> {
     let _bb = boxed.clone();
     boxed
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Point {
     x: i32,
     y: i32,
@@ -37,6 +91,24 @@ pub struct Point {
 
 impl Point {
     fn play(&self) {
+        println!("Point x: {}, y: {}", self.x, self.y);
+    }
+
+    fn play_ref(&self) {
+        println!("Point x: {}, y: {}", self.x, self.y);
+    }
+
+    fn play_mutref(&mut self) {
+        println!("Point x: {}, y: {}", self.x, self.y);
+    }
+    fn play_own(self) {
+        println!("Point x: {}, y: {}", self.x, self.y);
+    }
+    fn play_boxed(self: Box<Self>) {
+        println!("Point x: {}, y: {}", self.x, self.y);
+    }
+
+    fn play_arc(self: Arc<Self>) {
         println!("Point x: {}, y: {}", self.x, self.y);
     }
 }
@@ -47,3 +119,30 @@ fn foo2() -> Box<Point> {
     // let q = p;
     boxed
 }
+
+struct Atype {}
+struct Btype {}
+struct Ctype {}
+
+struct Mystruct {
+    #[allow(dead_code)]
+    a: Box<dyn Atrait>,
+}
+
+pub struct Mystruct2<'a> {
+    #[allow(dead_code)]
+    a: &'a dyn Atrait,
+}
+
+#[derive(Debug)]
+pub struct Point2 {
+    x: i32,
+    y: i32,
+}
+
+trait Atrait {}
+impl Atrait for Atype {}
+impl Atrait for Btype {}
+impl Atrait for Ctype {}
+
+fn doit(_t: Box<dyn Atrait>) {}
