af08d013cc08e56a0b95f0146913b32a2a8afd54
chenwochong <chenwochong@163.com>
2025-02-15 23:50:58 +0800

    rust 18

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,69 @@
-use tokio::sync::{mpsc, oneshot};
-use tokio::time;
+use std::fmt;
+use std::fs::File;
+use std::io::{Error, ErrorKind, Read};
+use std::net::IpAddr;
 
-#[tokio::main]
-async fn main() {
-    let ta = tokio::task::spawn(async {
-        println!("task a");
-        time::sleep(std::time::Duration::from_secs(3)).await;
-        1
-    });
+fn main() {
+    _ = foo();
+    let _custom_err = Error::new(ErrorKind::Other, "hello");
 
-    let tb = tokio::task::spawn(async {
-        println!("task b");
-        2
-    });
-    let tc = tokio::task::spawn(async {
-        println!("task c");
-        3
-    });
+    if let Err(e) = "127.0.0.1:8080".parse::<IpAddr>() {
+        println!("ip parse err {e}");
+    }
 
-    // let mut tasks = Vec::with_capacity(3);
-    // tasks.push(ta);
-    // tasks.push(tb);
-    // tasks.push(tc);
+    let res = read_file();
+    match res {
+        Ok(contents) => println!("{contents}"),
+        Err(e) => println!("{e}"),
+    }
+}
 
-    // let mut outputs = Vec::with_capacity(tasks.len());
-    // for (i, task) in tasks.into_iter().enumerate() {
-    //     println!("iter tasks {i}");
-    //     outputs.push(task.await.unwrap());
-    // }
+fn foo() -> Result<String, String> {
+    Ok("hahah".to_string())
+}
 
-    // println!("{:?}\n---------------\n", outputs);
+pub fn foo2(num: i32) -> Result<i32, String> {
+    if num == 10 {
+        Ok(num)
+    } else {
+        Err("other".to_string())
+    }
+}
 
-    // let (r1,r2, r3) = tokio::join!(ta, tb, tc);
-    // println!("{} {} {}",r1.unwrap(), r2.unwrap(), r3.unwrap());
+pub trait MyError: std::error::Error + std::fmt::Display {}
 
-    let ret = tokio::select! {
-        r = ta=> r.unwrap(),
-        r = tb=> r.unwrap(),
-        r = tc=> r.unwrap(),
-    };
+#[derive(Debug)]
+struct MyErr;
 
-    println!("got ret is {ret}");
-    println!("--------------");
+impl std::fmt::Display for MyErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "my err {}", self)
+    }
+}
 
-    let mut db = vec![1, 2, 3, 4, 5, 6, 7, 8];
+impl std::error::Error for MyErr {}
 
-    // let (tx, mut rx) = mpsc::channel::<i32>(100);
-    let (tx, mut rx) = mpsc::channel::<(i32, oneshot::Sender<bool>)>(100);
+fn foo3(num: i32) -> Result<i32, Box<dyn std::error::Error>> {
+    if num == 10 {
+        Ok(num)
+    } else {
+        let my_err = MyErr;
+        Err(Box::new(my_err))
+    }
+}
 
-    let tx_a = tx.clone();
-    let tx_b = tx.clone();
-
-    let task_a = tokio::task::spawn(async move {
-        println!("in task_a 1");
-        time::sleep(std::time::Duration::from_secs(1)).await;
-        println!("in task_a 2");
-        let (rsp_tx, rsp_rx) = oneshot::channel::<bool>();
-        if let Err(_) = tx_a.send((100, rsp_tx)).await {
-            println!("end task_a");
-        }
-
-        if let Ok(ret) = rsp_rx.await {
-            if ret {
-                println!("task_a fish with success");
-            } else {
-                println!("task_a fish with failure");
+fn read_file() -> Result<String, String> {
+    match File::open("/Users/wochong/Documents/code/hello/Cargo.toml").map_err(|e| format!("open file err {}", e)) {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            match file
+                .read_to_string(&mut contents)
+                .map_err(|_| "read file err".to_string())
+            {
+                Ok(_) => Ok(contents),
+                Err(e) => Err(e),
             }
-        } else {
-            println!("oneshot send dropped");
         }
-    });
-
-    let task_b = tokio::task::spawn(async move {
-        println!("in task_b");
-        let (rsp_tx, rsp_rx) = oneshot::channel::<bool>();
-        if let Err(_) = tx_b.send((100, rsp_tx)).await {
-            println!("end task_b");
-        }
-
-        if let Ok(ret) = rsp_rx.await {
-            if ret {
-                println!("task_b finish with success");
-            } else {
-                println!("task_b finish with failure");
-            }
-        } else {
-            println!("oneshot send dropped");
-        }
-    });
-
-    let task_c = tokio::task::spawn(async move {
-        while let Some((n, rsp_tx)) = rx.recv().await {
-            println!("got n = {n}");
-            db[5] = n;
-            println!("{:?}", db);
-            rsp_tx.send(true).unwrap();
-        }
-    });
-
-    _ = task_a.await;
-    _ = task_b.await;
-    _ = task_c.await;
+        Err(e) => Err(e),
+    }
 }
