bf1cd77952d7a219d43f4b476c652f1392592024
chenwochong <chenwochong@163.com>
2025-02-16 22:14:06 +0800

    rust 19

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::error::Error;
 use std::fmt;
 use std::fs::File;
-use std::io::{Error, ErrorKind, Read};
+use std::io::{Error as ioError, ErrorKind, Read};
 use std::net::IpAddr;
+use thiserror::Error as thisError;
 
 fn main() {
     _ = foo();
-    let _custom_err = Error::new(ErrorKind::Other, "hello");
+    let _custom_err = ioError::new(ErrorKind::Other, "hello");
 
     if let Err(e) = "127.0.0.1:8080".parse::<IpAddr>() {
         println!("ip parse err {e}");
@@ -15,6 +17,36 @@ fn main() {
     match res {
         Ok(contents) => println!("{contents}"),
         Err(e) => println!("{e}"),
+    }
+
+    let res = read_file2();
+    match res {
+        Ok(contents) => {
+            println!("{contents}");
+        }
+        Err(e) => {
+            println!("{e}");
+        }
+    }
+
+    let res = read_file3();
+    match res {
+        Ok(contents) => {
+            println!("{contents}");
+        }
+        Err(e) => {
+            println!("{e}");
+        }
+    }
+
+    let res = read_file4();
+    match res {
+        Ok(contents) => {
+            println!("{contents}");
+        }
+        Err(e) => {
+            println!("{}", e.0);
+        }
     }
 }
 
@@ -43,7 +75,7 @@ impl std::fmt::Display for MyErr {
 
 impl std::error::Error for MyErr {}
 
-fn foo3(num: i32) -> Result<i32, Box<dyn std::error::Error>> {
+pub fn foo3(num: i32) -> Result<i32, Box<dyn std::error::Error>> {
     if num == 10 {
         Ok(num)
     } else {
@@ -53,7 +85,10 @@ fn foo3(num: i32) -> Result<i32, Box<dyn std::error::Error>> {
 }
 
 fn read_file() -> Result<String, String> {
-    match File::open("/Users/wochong/Documents/code/hello/Cargo.toml").map_err(|e| format!("open file err {}", e)) {
+    println!("--------- read_file1 --------");
+    match File::open("/Users/wochong/Documents/code/hello/Cargo.toml")
+        .map_err(|e| format!("open file err {}", e))
+    {
         Ok(mut file) => {
             let mut contents = String::new();
             match file
@@ -66,4 +101,57 @@ fn read_file() -> Result<String, String> {
         }
         Err(e) => Err(e),
     }
+}
+
+fn read_file2() -> Result<String, String> {
+    println!("--------- read_file2 --------");
+    File::open("/Users/wochong/Documents/code/hello/Cargo.toml")
+        .map_err(|e| format!("open file err {e}"))
+        .and_then(|mut file| {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)
+                .map_err(|e| format!("read file err: {e}"))
+                .map(|num| {
+                    println!("contents len is {num}");
+                    contents
+                })
+        })
+}
+
+fn read_file3() -> Result<String, String> {
+    println!("--------- read_file3 --------");
+    let mut file = File::open("/Users/wochong/Documents/code/hello/Cargo.toml")
+        .map_err(|e| format!("open file err: {e}"))?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .map_err(|e| format!("read file err: {e}"))?;
+    Ok(contents)
+}
+
+pub struct MyString(String);
+
+impl From<std::io::Error> for MyString {
+    fn from(e: std::io::Error) -> Self {
+        MyString(format!("{e}"))
+    }
+}
+
+pub fn read_file4() -> Result<String, MyString> {
+    println!("--------- read_file4 --------");
+    let mut file = File::open("/Users/wochong/Documents/code/hello/Cargo.toml")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+#[derive(Debug, thisError)]
+pub enum DataStoreErr {
+    #[error("data is disconnet.")]
+    Disconnect(#[from] std::io::Error),
+    #[error("read action {0}")]
+    Redaction(String),
+    #[error("invalid error expected {expected:?}, found {found:?}")]
+    InvalidHeader { expected: String, found: String },
+    #[error("unknown error")]
+    Unknown,
 }
