6409788d589bc7e9824401fb1b08bf30273a6c1b
chenwochong <chenwochong@163.com>
2025-02-24 20:01:49 +0800

    rust 22

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use axum::{
     extract::{rejection::JsonRejection, Form, Json, Path, Query, State},
     http::StatusCode,
     response::{Html, IntoResponse, Redirect},
-    routing::{get, post},
+    routing::{delete, get, post, put},
     Router,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tower_http::services::{ServeDir, ServeFile};
 use tower_http::trace::TraceLayer;
@@ -14,6 +14,7 @@ use tower_http::trace::TraceLayer;
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
 use tokio_postgres::NoTls;
+use uuid::Uuid;
 
 type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 
@@ -22,7 +23,7 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let manager = PostgresConnectionManager::new_from_stringlike(
-        "host=pull-image user=test dbname=test password=Test_123",
+        "host=114.116.219.50 user=test port=31432 dbname=todolist password=Test_123",
         NoTls,
     )
     .unwrap();
@@ -41,15 +42,17 @@ async fn main() {
         .route("/form", post(accept_form))
         .route("/json", post(accept_json))
         .route("/user", post(create_user))
+        .route("/todos", get(todos_list))
+        .route("/todo", post(todo_create))
+        .route("/todo/{id}", delete(todo_delete))
+        .route("/todo", put(todo_update))
         .nest_service("/assets2", serve_dir)
         .nest_service("/assets", ServeDir::new("assets"))
         .layer(TraceLayer::new_for_http())
         .fallback(hello)
         .with_state(pool);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("http listen on: {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
@@ -150,4 +153,113 @@ where
     E: std::error::Error,
 {
     (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
+}
+
+#[derive(Debug, Serialize)]
+struct Todo {
+    id: String,
+    description: String,
+    completed: bool,
+}
+
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<i64>,
+    pub limit: Option<i64>,
+}
+
+async fn todos_list(
+    State(pool): State<ConnectionPool>,
+    pagination: Query<Pagination>,
+) -> Result<Json<Vec<Todo>>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+    let offset = pagination.offset.unwrap_or(0);
+    let limit = pagination.limit.unwrap_or(100);
+
+    let rows = conn
+        .query("select * from todo offset $1 limit $2", &[&offset, &limit])
+        .await
+        .map_err(internal_error)?;
+
+    let mut todos = Vec::<Todo>::new();
+    for row in rows {
+        let id = row.get(0);
+        let description = row.get(1);
+        let completed = row.get(2);
+        let todo = Todo {
+            id,
+            description,
+            completed,
+        };
+        todos.push(todo);
+    }
+    Ok(Json(todos))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTodo {
+    pub description: String,
+}
+async fn todo_create(
+    State(pool): State<ConnectionPool>,
+    Json(input): Json<CreateTodo>,
+) -> Result<(StatusCode, Json<Todo>), (StatusCode, String)> {
+    let todo = Todo {
+        id: Uuid::new_v4().simple().to_string(),
+        description: input.description,
+        completed: false,
+    };
+
+    let conn = pool.get().await.map_err(internal_error)?;
+    let _ret = conn
+        .execute(
+            "insert into todo (id, description, completed) values ($1, $2, $3)",
+            &[&todo.id, &todo.description, &todo.completed],
+        )
+        .await
+        .map_err(internal_error)?;
+    Ok((StatusCode::CREATED, Json(todo)))
+}
+async fn todo_delete(
+    State(pool): State<ConnectionPool>,
+    Path(id): Path<String>,
+) -> Result<(StatusCode, Json<String>), (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+    let _ret = conn
+        .execute("delete from todo where id=$1", &[&id])
+        .await
+        .map_err(internal_error)?;
+    Ok((StatusCode::OK, Json(id)))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateTodo {
+    pub id: String,
+    pub description: Option<String>,
+    pub completed: Option<bool>,
+}
+async fn todo_update(
+    State(pool): State<ConnectionPool>,
+    Json(input): Json<UpdateTodo>,
+) -> Result<(StatusCode, Json<String>), (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+
+    let id = input.id.clone();
+    let description = input.description.unwrap_or("".to_string());
+    let completed = input.completed.unwrap_or(false);
+    tracing::debug!(
+        "id, description, completed is:  {}, {}, {}",
+        id,
+        description,
+        completed
+    );
+
+    let _ret = conn
+        .execute(
+            "update todo set description=$1, completed=$2 where id=$3",
+            &[&description, &completed, &id],
+        )
+        .await
+        .map_err(internal_error)?;
+    Ok((StatusCode::OK, Json(input.id)))
 }
