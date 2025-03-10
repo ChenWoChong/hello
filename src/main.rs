463a17b9e7110988fda5974af7ca6e9719fe9b87
chenwochong <chenwochong@163.com>
2025-03-10 11:49:58 +0800

    rust 28

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::ops::Range;
-fn main() {
-    assert_eq!((1..5), Range { start: 1, end: 5 });
+use nom::IResult;
+use std::error::Error;
+
+pub fn do_nothing_parser(input: &str) -> IResult<&str, &str> {
+    Ok((input, ""))
 }
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut mp = std::collections::HashMap::<i32, i32>::new();
-    for (idx, &num) in nums.iter().enumerate() {
-        if let Some(&i) = mp.get(&(target - num)) {
-            return vec![idx as i32, i];
-        }
-        mp.insert(num, idx as i32);
-    }
-    unreachable!()
+fn main() -> Result<(), Box<dyn Error>> {
+    let str = "abcdefg";
+    let (remainning_input, output) = do_nothing_parser(str)?;
+    assert_eq!(remainning_input, str);
+    assert_eq!(output, "");
+    Ok(())
 }
