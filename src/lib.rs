20abf33f0f32386e3895b0516b76acd667835025
chenwochong <chenwochong@163.com>
2025-03-12 17:31:56 +0800

    rust 30

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
+use pyo3::prelude::*;
+
+#[no_mangle]
+pub extern "C" fn hello_from_rust() {
+    println!("hello from Rust!");
+}
+
+fn fib(n: usize) -> BigUint {
+    let mut f0: BigUint = Zero::zero();
+    let mut f1: BigUint = One::one();
+    for _ in 0..n {
+        let f2 = f0 + &f1;
+        f0 = f1;
+        f1 = f2;
+    }
+    f0
+}
+
+#[pyfunction]
+fn calc_fib(n: usize) -> PyResult<()> {
+    let _ = fib(n);
+    Ok(())
+}
+
+#[pymodule]
+fn rust_fib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(calc_fib, m)?)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,35 @@
-use std::{slice, str};
-#[allow(static_mut_refs)]
+use std::fmt;
+
+#[link(name = "m")]
+extern "C" {
+    fn csqrtf(z: Complex) -> Complex;
+    fn ccosf(z: Complex) -> Complex;
+}
+
+fn cos(z: Complex) -> Complex {
+    unsafe { ccosf(z) }
+}
+
 fn main() {
-    let mut u = IntOrFloat { f: 1.0 };
-    assert_eq!(unsafe { u.i }, 1065353216);
-    u.i = 1073741824;
-    assert_eq!(unsafe { u.f }, 2.0);
-
-    let v = [1, 2, 3];
-    add_counter(6);
-    unsafe {
-        println!("COUNTER: {}", COUNTER);
-        // println!("{}", v[3]);
-        println!("v[2]: {}", v[2]);
-    }
-
-    let num: i32 = 10;
-    let num_ptr: *const i32 = &num;
-    let mut nu: i32 = 11;
-    let mut_nu_ptr: *mut i32 = &mut nu;
-    unsafe {
-        println!("num {}", *num_ptr);
-        println!("nu {}", *mut_nu_ptr);
-    }
-
-    let tt = Box::new(66);
-    let tt = Box::into_raw(tt);
-    unsafe {
-        let _ = Box::from_raw(tt);
-    }
-
-    use std::ptr;
-
-    let null_v: *const i32 = ptr::null();
-    assert!(null_v.is_null());
-
-    let mut_null: *mut i32 = ptr::null_mut();
-    assert!(mut_null.is_null());
-
-    aa();
-
-    let mut ass = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("ass:\t {:?}", ass);
-    let (l, r) = split_slice(&mut ass, 3);
-    println!("left:\t {:?}", l);
-    println!("right:\t {:?}", r);
-    println!("ass:\t {:?}", ass);
-
-    unsafe {
-        assert_eq!(ass.get_unchecked(2), &3);
-    }
-
-    let heart = vec![240, 159, 146, 150];
-
-    let heart_str = unsafe { str::from_utf8_unchecked(&heart) };
-    assert_eq!("💖", heart_str);
-    println!("heart:\t {}", heart_str);
+    let z = Complex { re: -1., im: 0. };
+    let z_sqrt = unsafe { csqrtf(z) };
+    println!("The square root of {:?} is {:?}", z, z_sqrt);
+    println!("cos({:?}) = {:?}", z, cos(z));
 }
 
-fn aa() {
-    let a = &10 as *const i32;
-    let b = &mut 11 as *mut i32;
-
-    unsafe {
-        println!("a {}", *a);
-        println!("b {}", *b);
-    }
-}
-union IntOrFloat {
-    i: u32,
-    f: f32,
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct Complex {
+    re: f32,
+    im: f32,
 }
 
-static mut COUNTER: i32 = 30;
-
-fn add_counter(inc: i32) {
-    unsafe {
-        COUNTER += inc;
-    }
-}
-
-fn split_slice(v: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = v.len();
-    let ptr = v.as_mut_ptr();
-    assert!(mid <= len);
-    unsafe {
-        (
-            slice::from_raw_parts_mut(ptr, mid),
-            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
-        )
+impl fmt::Debug for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.im < 0. {
+            write!(f, "{}-{}", self.re, -self.im)
+        } else {
+            write!(f, "{}+{}", self.re, self.im)
+        }
     }
 }
